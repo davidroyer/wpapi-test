c2f058787c5344d510931196bdcd892c62d1334b
David Royer <droyer01@gmail.com>
2018-11-17 07:37:55 -0500

    chore(wp-client): Reorganized directory structure

---
+++ packages/wp-client/src/utils/default-resources.ts
@@ -0,0 +1,48 @@
+// Predefined resources.
+export default [
+    {
+      collectionName: 'categories',
+      singleName: 'category'
+    },
+    {
+      collectionName: 'comments',
+      singleName: 'comment'
+    },
+    {
+      collectionName: 'media',
+      singleName: 'media'
+    },
+    {
+      collectionName: 'menus',
+      singleName: 'menu'
+    },
+    {
+      collectionName: 'pages',
+      singleName: 'page'
+    },
+    {
+      collectionName: 'posts',
+      singleName: 'post'
+    },  
+    {
+      collectionName: 'tags',
+      singleName: 'tag'
+    },
+    {
+      collectionName: 'taxonomies',
+      singleName: 'taxonomy'
+    },
+    {
+      collectionName: 'statuses',
+      singleName: 'status'
+    },
+    {
+      collectionName: 'types',
+      singleName: 'type'
+    },
+    {
+      collectionName: 'users',
+      singleName: 'user'
+    }
+  ]
+  
